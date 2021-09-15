d131419df69ff50bf94d2e6b0eb2c6910e6ceff8
Tatiana Pavlova <pavlova.tat.ole@gmail.com>
2021-09-15 09:31:45 +0300

    refactor: correct last review remarks

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -28,18 +28,16 @@ function AddPlacePopup (props) {
   return (
     <PopupWithForm name="new-card" title="Новое место" isOpen={props.isOpen} onClose={props.onClose} onSubmit={handleSubmit} 
                         buttonTitle="Создать">
-      <>
-        <div className="popup__input-field">
-          <input id="place-input" type="text" value={cardName} onChange={handleCardNameChange} placeholder="Название" 
-                  className="popup__input popup__input_place-name" name="placeName" required minLength="2" maxLength="30"/>
-          <span className="place-input-error"></span>
-        </div>
-        <div className="popup__input-field">
-          <input id="link-input" type="url" value={cardUrl} onChange={handleCardUrlChange} placeholder="Ссылка на картинку"
-                  className="popup__input popup__input_place-link" name="placeLink" required/>
-          <span className="link-input-error"></span>
-        </div>
-      </>
+      <div className="popup__input-field">
+        <input id="place-input" type="text" value={cardName} onChange={handleCardNameChange} placeholder="Название" 
+                className="popup__input popup__input_place-name" name="placeName" required minLength="2" maxLength="30"/>
+        <span className="place-input-error"></span>
+      </div>
+      <div className="popup__input-field">
+        <input id="link-input" type="url" value={cardUrl} onChange={handleCardUrlChange} placeholder="Ссылка на картинку"
+                className="popup__input popup__input_place-link" name="placeLink" required/>
+        <span className="link-input-error"></span>
+      </div>
     </PopupWithForm>
   )
 }
--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -35,18 +35,16 @@ function EditProfilePopup (props) {
   return (
     <PopupWithForm name="edit-profile" title="Редактировать профиль" isOpen={props.isOpen} onClose={props.onClose} onSubmit={handleSubmit}
                         buttonTitle="Сохранить"> 
-      <>
-        <div className="popup__input-field">
-          <input id="name-input" type="text" value={name} onChange={handleNameChange} className="popup__input popup__input_edit_name" name="name"
-                placeholder="Имя" required minLength="2" maxLength="40"/>
-          <span className="name-input-error"></span>
-        </div>
-        <div className="popup__input-field">
-          <input id="job-input" type="text" value={description} onChange={handleDescriptionChange} className="popup__input popup__input_edit_job" name="job" 
-                placeholder="Вид деятельности" required minLength="2" maxLength="200"/>
-          <span className="job-input-error"></span>
-        </div>
-      </>
+      <div className="popup__input-field">
+        <input id="name-input" type="text" value={name} onChange={handleNameChange} className="popup__input popup__input_edit_name" name="name"
+              placeholder="Имя" required minLength="2" maxLength="40"/>
+        <span className="name-input-error"></span>
+      </div>
+      <div className="popup__input-field">
+        <input id="job-input" type="text" value={description} onChange={handleDescriptionChange} className="popup__input popup__input_edit_job" name="job" 
+              placeholder="Вид деятельности" required minLength="2" maxLength="200"/>
+        <span className="job-input-error"></span>
+      </div>
     </PopupWithForm> 
   );
 }
